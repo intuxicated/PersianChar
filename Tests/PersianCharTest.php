ff94a06348b177370c6ba5f9c5c0c9526a8590a1
intuxicated <hidensoft@gmai.com>
2014-07-21 14:12:33 +0430

    init commit

---
+++ Tests/PersianCharTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Intuxicated\PersianChar\Tests;
+
+use Intuxicated\PersianChar\PersianChar;
+
+class PersianCharTest extends \PHPUnit_Framework_TestCase {
+
+    public function testNumbers(){
+        $r = PersianChar::ar_numbers('۱۲۳۴1234١٢٣٤');
+        $this->assertEquals($r,'۱۲۳۴1234۱۲۳۴');
+        $r = PersianChar::en_numbers('۱۲۳۴1234١٢٣٤');
+        $this->assertEquals($r,'۱۲۳۴۱۲۳۴١٢٣٤');
+        $r = PersianChar::numbers('۱۲۳۴1234١٢٣٤');
+        $this->assertEquals($r,'۱۲۳۴۱۲۳۴۱۲۳۴');
+        $r = PersianChar::numbers(123456);
+        $this->assertEquals($r,'۱۲۳۴۵۶');
+    }
+
+    public function testLetters(){
+        $r = PersianChar::letters('ؠک مټن تﺴت');
+        $this->assertEquals($r,'یک متن تست');
+        $r = PersianChar::letters('﷼ ﷲ');
+        $this->assertEquals($r,'ریال الله');
+    }
+}
+ 
